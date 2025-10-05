02d1b0a26f4f13230956e9cfb2288c61c4940dbf
diegoalrv <d.alexis.ramirez@gmail.com>
2025-10-05 05:04:29 -0300

    change to iconlayer

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,43 +1,54 @@
 import DeckGL from '@deck.gl/react';
-import { ScatterplotLayer } from '@deck.gl/layers';
+import { IconLayer } from '@deck.gl/layers';
 import Map, { NavigationControl } from 'react-map-gl';
 
 const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 
 export default function MapView({ assignments }) {
+  const features = assignments.features || [];
+
   const layers = [
-    new ScatterplotLayer({
-      id: 'pois',
-      data: assignments.map(a => ({
-        ...a,
-        coordinates: parseWKT(a.poi.wkt_geometry)
+    new IconLayer({
+      id: 'pois-icons',
+      data: features.map(f => ({
+        ...f.properties,
+        coordinates: f.geometry.coordinates
       })),
       getPosition: d => d.coordinates,
-      getFillColor: [255, 140, 0],
-      getRadius: 50,
-      pickable: true
+      getIcon: () => 'marker',
+      sizeScale: 5,
+      sizeUnits: 'pixels',
+      getSize: () => 40,
+      getColor: [255, 200, 0], // un amarillo más brillante que contraste en fondo oscuro
+      pickable: true,
+      onClick: info => {
+        if (info.object) {
+          alert(`POI: ${info.object.name}`);
+        }
+      },
+      iconAtlas:
+        'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+      iconMapping:
+        'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.json'
     })
   ];
 
   return (
-    <DeckGL
-      initialViewState={{ longitude: -73.05, latitude: -36.82, zoom: 13 }}
-      controller={true}
-      layers={layers}
-    >
-      <Map
-        mapboxAccessToken={MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        style={{ width: '100%', height: '100%' }}
+    <div style={{ height: '100vh', backgroundColor: '#121212', color: '#f0f0f0' }}>
+      <DeckGL
+        initialViewState={{ longitude: -73.05, latitude: -36.82, zoom: 13 }}
+        controller={true}
+        layers={layers}
       >
-        <NavigationControl position="top-left" />
-      </Map>
-    </DeckGL>
+        <Map
+          mapboxAccessToken={MAPBOX_TOKEN}
+          // 🔥 Tema oscuro de Mapbox
+          mapStyle="mapbox://styles/mapbox/dark-v11"
+          style={{ width: '100%', height: '100%' }}
+        >
+          <NavigationControl position="top-left" />
+        </Map>
+      </DeckGL>
+    </div>
   );
-}
-
-function parseWKT(wkt) {
-  if (!wkt?.startsWith('POINT')) return [0, 0];
-  const coords = wkt.replace('POINT', '').replace(/[()]/g, '').trim().split(' ');
-  return [parseFloat(coords[0]), parseFloat(coords[1])];
 }
